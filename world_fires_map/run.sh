fa61158b3adb38de595df7f0fc55bc37a518fbfa
Donzaffi <47207943+Donzaffi@users.noreply.github.com>
2026-07-06 17:14:12 +0200

    Update run.sh

--- world_fires_map/run.sh
+++ world_fires_map/run.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/with-contenv bashio
-# Liest die Werte direkt aus der HA-Konfiguration
+
 export NASA_API_KEY=$(bashio::config 'nasa_api_key')
-export LAT=$(bashio::config 'latitude')
-export LON=$(bashio::config 'longitude')
-export RAD=$(bashio::config 'radius')
+export LATITUDE=$(bashio::config 'latitude')
+export LONGITUDE=$(bashio::config 'longitude')
+export RADIUS=$(bashio::config 'radius')
+
+
+echo "Starting World Fires Map application..."
 
 exec python3 /app/app.py
