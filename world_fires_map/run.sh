08779fec1344d0728c6c0069b8e3dc857faf0dc3
Donzaffi <47207943+Donzaffi@users.noreply.github.com>
2026-07-06 15:59:33 +0200

    Update run.sh

--- world_fires_map/run.sh
+++ world_fires_map/run.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-# Get the API Key from Home Assistant configuration
-export NASA_API_KEY=$(bashio::config 'nasa_api_key')
+# Note: NASA_API_KEY is automatically injected by Home Assistant
+# as an environment variable based on config.json
 
 echo "Starting World Fires Map..."
-
-# Simple python execution as PID 1
 python3 /app/app.py
