4daf4e52d6c75d01a99ace4ed22f26fc81dfb3a8
Donzaffi <47207943+Donzaffi@users.noreply.github.com>
2026-07-06 15:44:35 +0200

    Update run.sh

--- world_fires_map/run.sh
+++ world_fires_map/run.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/with-contenv bashio
 
-# Use bashio to get the configuration and set it as an environment variable
-# This ensures it is available to the python script
+# Set the API key using bashio
 export NASA_API_KEY=$(bashio::config 'nasa_api_key')
 
 echo "Starting World Fires Map application..."
 
-# Run python directly. We do not need sudo or suexec here, 
-# as the container runs as root by default.
-python3 /app/app.py
+# Run the application
+exec python3 /app/app.py
